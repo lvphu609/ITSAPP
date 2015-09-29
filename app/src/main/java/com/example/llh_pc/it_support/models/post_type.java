f5d36322afb164a28d6497e48698dc13d8394fc3
Hai <leelamhair@gmail.com>
2015-09-29 18:13:11 +0700

    completed detail post

---
+++ app/src/main/java/com/example/llh_pc/it_support/models/post_type.java
@@ -0,0 +1,53 @@
+package com.example.llh_pc.it_support.models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by LLH-PC on 9/29/2015.
+ */
+public class post_type {
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    @SerializedName("id")
+    public String id;
+
+    @SerializedName("name")
+    public String name;
+
+    @SerializedName("description")
+    public String description;
+
+    @SerializedName("avatar")
+    public String avatar;
+}
--- app/src/main/java/com/example/llh_pc/it_support/utils/Events/eventDetailPost.java
+++ app/src/main/java/com/example/llh_pc/it_support/utils/Events/eventDetailPost.java
@@ -1,25 +1,83 @@
 package com.example.llh_pc.it_support.utils.Events;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.AdapterView;
 
+import com.example.llh_pc.it_support.activities.frmChiTietPost;
+import com.example.llh_pc.it_support.activities.frmDK_DN;
+import com.example.llh_pc.it_support.models.JsonParses.LoginParse;
+import com.example.llh_pc.it_support.models.JsonParses.PostDetailParse;
+import com.example.llh_pc.it_support.models.JsonParses.PostParse;
 import com.example.llh_pc.it_support.models.Post;
+import com.example.llh_pc.it_support.models.PostDetail;
+import com.example.llh_pc.it_support.models.UserPostDetail;
+import com.example.llh_pc.it_support.restclients.RequestMethod;
+import com.example.llh_pc.it_support.restclients.Response;
+import com.example.llh_pc.it_support.restclients.RestClient;
 import com.example.llh_pc.it_support.utils.Interfaces.Def;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Created by LLH-PC on 9/27/2015.
  */
 public class eventDetailPost implements AdapterView.OnItemClickListener {
 
-    public static final String url_get_my_notifications = Def.API_BASE_LINK + Def.API_PostTile + Def.API_FORMAT_JSON;
+    public static final String url_get= Def.API_BASE_LINK + Def.API_Loadpostdetail + Def.API_FORMAT_JSON;
     private Context context;
-    private ArrayList<Post> arrayListPost;
-
+    private ArrayList<PostDetail> arrayListPost;
+    private PostDetail postDetail;
+    private UserPostDetail uD;
+    public eventDetailPost(Context current,ArrayList<PostDetail> list )
+    {
+        this.context = current;
+        this.arrayListPost = list;
+    }
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+        PostDetail p = arrayListPost.get(position);
+        String idPost = p.getId();
+        try {
+            SharedPreferences sharedPreference = PreferenceManager.getDefaultSharedPreferences(context);
+            String token = sharedPreference.getString("token", "token");
+            RestClient restClient = new RestClient(url_get);
+            restClient.addBasicAuthentication(Def.API_USERNAME_VALUE, Def.API_PASSWORD_VALUE);
+            restClient.addHeader("token", token);
+            restClient.addParam("id",idPost);
+            restClient.execute(RequestMethod.POST);
+            if (restClient.getResponseCode() == Def.RESPONSE_CODE_SUCCESS) {
+                String jsonObject = restClient.getResponse();
+                Gson gson = new Gson();
+                PostDetailParse getLoginJson = gson.fromJson(jsonObject, PostDetailParse.class);
+                if (getLoginJson.getStatus().equalsIgnoreCase(Response.STATUS_SUCCESS)) {
+                    uD = getLoginJson.getResults();
+                    String loaibaohong = uD.post_type.getName();
+                    String diachi = uD.location_name;
+                    String ghichu = uD.content;
+                    String hoten = uD.normal_account.full_name;
+                    String dienthoai= uD.normal_account.phone_number;
+                    String diachinha = uD.normal_account.address;
+                    Intent intent = new Intent(context, frmChiTietPost.class);
+                    intent.putExtra( "loaibaohong", loaibaohong );
+                    intent.putExtra( "diachi", diachi );
+                    intent.putExtra( "ghichu", ghichu );
+                    intent.putExtra( "hoten", hoten );
+                    intent.putExtra( "dienthoai", dienthoai );
+                    intent.putExtra( "diachinha", diachinha );
+                    context.startActivity(intent);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
