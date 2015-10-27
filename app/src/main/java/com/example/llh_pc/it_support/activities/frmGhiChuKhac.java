c35785576a68b9d977a1ae3b6e37498d3cfa1181
Hai <leelamhair@gmail.com>
2015-10-27 17:14:10 +0700

    create post

--- app/src/main/java/com/example/llh_pc/it_support/activities/frmGhiChuKhac.java
+++ app/src/main/java/com/example/llh_pc/it_support/activities/frmGhiChuKhac.java
@@ -2,17 +2,13 @@ package com.example.llh_pc.it_support.activities;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
-import android.provider.Settings;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,7 +27,8 @@ import com.example.llh_pc.it_support.utils.Interfaces.Def;
 import com.example.llh_pc.it_support.utils.Location.GPSTracker;
 import com.google.gson.Gson;
 
-public class frmGhiChu extends AppCompatActivity {
+public class frmGhiChuKhac extends AppCompatActivity {
+
     public static final String url_get_my_notifications = Def.API_BASE_LINK + Def.API_CREATESubPost + Def.API_FORMAT_JSON;
     private GPSTracker gpsTracker;
     private boolean isGPS;
@@ -42,14 +39,14 @@ public class frmGhiChu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frm_ghi_chu);
+        setContentView(R.layout.activity_frm_ghi_chu_khac);
         /*final EditText editGhiChu = (EditText)findViewById(R.id.edtGhiChu);*/
         Button btnOK = (Button) findViewById(R.id.btnCreatePost);
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final EditText editGhiChu = (EditText) findViewById(R.id.edtGhiChu);
-                SharedPreferences sharedPreference = PreferenceManager.getDefaultSharedPreferences(frmGhiChu.this);
+                SharedPreferences sharedPreference = PreferenceManager.getDefaultSharedPreferences(frmGhiChuKhac.this);
                 String token = sharedPreference.getString("token", "YourName");
                 String type_id = sharedPreference.getString("type_id", "YourName");
                 String content = editGhiChu.getText().toString();
@@ -57,7 +54,7 @@ public class frmGhiChu extends AppCompatActivity {
                 if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
                     buildAlertMessageNoGps();
                 } else {
-                    gpsTracker = new GPSTracker(frmGhiChu.this);
+                    gpsTracker = new GPSTracker(frmGhiChuKhac.this);
                     String Longitude = String.valueOf(gpsTracker.getLongitude());
                     String Latitude = String.valueOf(gpsTracker.getLatitude());
                     new NoteAsyncTack().execute(token, type_id, content, Longitude, Latitude);
@@ -116,10 +113,10 @@ public class frmGhiChu extends AppCompatActivity {
         });
         getSupportActionBar().setHomeAsUpIndicator(R.mipmap.back);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+    }
     private void buildAlertMessageNoGps() {
-        LayoutInflater li = LayoutInflater.from(frmGhiChu.this);
+        LayoutInflater li = LayoutInflater.from(frmGhiChuKhac.this);
         View promptsView = li.inflate(R.layout.popup_note, null);
         android.support.v7.app.AlertDialog.Builder alertDialogBuilder = new android.support.v7.app.AlertDialog.Builder(this);
         alertDialogBuilder.setView(promptsView);
@@ -134,7 +131,7 @@ public class frmGhiChu extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                Toast.makeText(frmGhiChu.this, "Mở GPS.", Toast.LENGTH_LONG).show();
+                Toast.makeText(frmGhiChuKhac.this, "Mở GPS.", Toast.LENGTH_LONG).show();
                 show.dismiss();
             }
         });
@@ -147,52 +144,30 @@ public class frmGhiChu extends AppCompatActivity {
         });
     }
 
-    /* private void buildAlertMessageNoGps() {
-         LayoutInflater li = LayoutInflater.from(frmGhiChu.this);
-         View promptsView = li.inflate(R.layout.popup_validation, null);
-         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(frmGhiChu.this);
-         alertDialogBuilder.setView(promptsView);
-         final TextView textView = (TextView) promptsView.findViewById(R.id.tvValidation);
-         textView.setText("IT Support cần xác định vị trí của bạn. Bạn có muốn bật GPS");
-         // set dialog message
-         alertDialogBuilder
-                 .setTitle("IT Support")
-                 .setCancelable(false)
-                 .setPositiveButton("OK",
-                         new DialogInterface.OnClickListener() {
-                             public void onClick(DialogInterface dialog, int id) {
-                                 startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                                 Toast.makeText(frmGhiChu.this, "Mở GPS.", Toast.LENGTH_LONG).show();
-                             }
-                         });
-         alertDialogBuilder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
-
-             @Override
-             public void onClick(DialogInterface dialog, int which) {
-                 // Do nothing
-                 dialog.dismiss();
-             }
-         });
-
-         // create alert dialog
-         AlertDialog alertDialog = alertDialogBuilder.create();
-         // show it
-         alertDialog.show();
-
-     }*/
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_frm_ghi_chu_khac, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     private class NoteAsyncTack extends AsyncTask<String, Void, String> {
         private String jsonObject;
+        private ProgressDialog progressDialog;
 
         /*function
                 * 1: GPS
@@ -200,7 +175,7 @@ public class frmGhiChu extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            progressDialog = ProgressDialog.show(frmGhiChu.this, "IT Support", "Loading...");
+            progressDialog = ProgressDialog.show(frmGhiChuKhac.this, "IT Support", "Loading...");
         }
 
         @Override
@@ -244,12 +219,12 @@ public class frmGhiChu extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             if (s.equals("1")) {
-                Toast.makeText(frmGhiChu.this, "Báo hỏng thành công.", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(frmGhiChu.this, frmTabHost.class);
+                Toast.makeText(frmGhiChuKhac.this, "Báo hỏng thành công.", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(frmGhiChuKhac.this, frmTabHost.class);
                 startActivity(intent);
                 frmTabHost.x = 3;
             } else {
-                Intent intent = new Intent(frmGhiChu.this, frmDK_DN.class);
+                Intent intent = new Intent(frmGhiChuKhac.this, frmDK_DN.class);
                 startActivity(intent);
             }
         }
